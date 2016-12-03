bb5cc336d38490ef460c75704045b1565d74f6cf
GarbageKing <garbageking@mail.com>
2016-12-03 13:21:38 +0200

    collapsing navbar added for constructed pages

--- web/js/newjs.js
+++ web/js/newjs.js
@@ -27,7 +27,15 @@ $(document).ready(function(){
     $('#add-header').click(function(){
        
        header = 
-        '<a class="navbar-brand" href="#">'+$('#comp-name').val()+'</a>'+
+               '<div class="navbar-header">'+
+                '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" >'+
+                    '<span class="icon-bar" style="border:1px solid #000"></span>'+
+                    '<span class="icon-bar" style="border:1px solid #000"></span>'+
+                    '<span class="icon-bar" style="border:1px solid #000"></span>'+
+                '</button>'+
+                  '<a class="navbar-brand" href="#">'+$('#comp-name').val()+'</a>'+
+              '</div>'+  
+        '<div class="collapse navbar-collapse" id="myNavbar">'+
         '<ul class="nav navbar-nav">'+
             '<li class="nav-item active">'+
             '<a class="nav-link" href="'+$('#menu-1-href').val()+'">'+$('#menu-1').val()+'</a>'+
@@ -41,8 +49,8 @@ $(document).ready(function(){
          '</li>'+'<li class="nav-item active">'+
            '<a class="nav-link" href="'+$('#menu-5-href').val()+'">'+$('#menu-5').val()+'</a>'+
          '</li>'+    
-        '</ul>';
-        
+        '</ul>'+
+        '</div>';
     });
     
     $('#add-title').click(function(){
@@ -156,6 +164,8 @@ $(document).ready(function(){
                 '<meta charset="UTF-8">' +              
                 '<meta name="viewport" content="width=device-width, initial-scale=1">' + 
                 '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">'+
+                '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>'+
+                '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>'+
                 style + foostyle + '</style>' +
             '</head>' +     
             '<body>' + 
